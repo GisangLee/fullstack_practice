67236e2011a8ebd364919a3eb819022921bbb9b1
DaeLee <48057905+GisangLee@users.noreply.github.com>
2021-12-08 13:09:20 +0900

    count Amenities

--- rooms/admin.py
+++ rooms/admin.py
@@ -12,7 +12,71 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
 
     """ 숙박 Admin Definition """
-    list_display = admin.ModelAdmin.list_display + ("address", "check_in", "check_out", "host",)
+    list_display = admin.ModelAdmin.list_display + (
+        "address",
+        "check_in",
+        "check_out",
+        "host",
+        "room_name",
+        "country",
+        "city",
+        "price",
+        "beds",
+        "bedrooms",
+        "baths",
+        "guests",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ("room_name", "price",)
+
+    list_filter = (
+        "instant_book",
+        "city",
+        "roomtype",
+        "amenities",
+        "facilities",
+        "houserules",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username",)
+
+    filter_horizontal = ("amenities", "facilities", "houserules",)
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("room_name", "description", "country", "address", "price",)}
+        ),
+
+        (
+            "Times",
+            {"fields": ("check_in", "check_out", "instant_book",)}
+        ),
+
+        (
+            "Spaces",
+            {"fields": ("beds", "bedrooms", "baths", "guests",)}
+        ),
+
+        (
+            "More About Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "houserules",)
+            }
+        ),
+
+        (
+            "Last Details",
+            {"fields": ("host",)}
+        )
+    )
+
+    def count_amenities(self, obj):
+        return "Potato"
 
 
 @admin.register(models.Photo)
